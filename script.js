55477621831748c1d5965366333ae803e87a06e4
Elia Schneider <elyssch@gmail.com>
2024-12-24 15:09:49 +0100

    all song requested added

--- script.js
+++ script.js
@@ -54,4 +54,116 @@ fetch(eminemAPI)
 })
 .catch(error => console.log('Something wen wrong = ' + error))
  
-let test 
+
+//--------------  METALLICA ------------------//
+
+let metallicaAPI = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica'
+let metallicaSection = document.getElementById('metallicaSection')
+
+fetch(metallicaAPI)
+.then(metallicaAPIRaw => metallicaAPIRaw.json())
+.then(metallicaAPIRefined => {console.log(metallicaAPIRefined)
+
+    console.log(metallicaAPIRefined.data [0]['artist']['name'])
+    console.log(metallicaAPIRefined.data [0]['album']['cover_medium'])
+    
+   let metallicaAlbum= metallicaAPIRefined.data
+   
+
+
+    for (i=0; i<metallicaAlbum.length; i++){
+
+        console.log(metallicaAlbum[i])
+        console.log(metallicaAlbum[i]['album']['cover_medium'])
+        let coverAlbum = metallicaAlbum[i]['album']['cover_medium']
+
+        let card = document.createElement('div')
+    card.className = "card"
+    metallicaSection.appendChild(card)
+
+    let cover = document.createElement('div')
+    cover.className = "cover"
+    cover.innerHTML = '<img class="img-fluid imgC" src= ' + coverAlbum + '>'
+    card.appendChild(cover)
+
+    let title = document.createElement('h5')
+    title.className = 'title'
+    title.innerText ='Title'
+    card.appendChild(title)
+
+        
+
+    }
+
+    let card = document.createElement('div')
+    card.className = "card"
+    metallicaSection.appendChild(card)
+
+    let cover = document.createElement('div')
+    cover.className = "cover"
+    card.appendChild(cover)
+
+    let title = document.createElement('h5')
+    title.className = 'title'
+    title.innerText ='Title'
+    card.appendChild(title)
+
+})
+.catch(error => console.log('Something wen wrong = ' + error))
+
+
+//--------------  queen ------------------//
+
+let queenAPI = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=queen'
+let queenSection = document.getElementById('queenSection')
+
+fetch(queenAPI)
+.then(queenAPIRaw => queenAPIRaw.json())
+.then(queenAPIRefined => {console.log(queenAPIRefined)
+
+    console.log(queenAPIRefined.data [0]['artist']['name'])
+    console.log(queenAPIRefined.data [0]['album']['cover_medium'])
+    
+   let queenAlbum= queenAPIRefined.data
+   
+
+
+    for (i=0; i<queenAlbum.length; i++){
+
+        console.log(queenAlbum[i])
+        console.log(queenAlbum[i]['album']['cover_medium'])
+        let coverAlbum = queenAlbum[i]['album']['cover_medium']
+
+        let card = document.createElement('div')
+    card.className = "card"
+    queenSection.appendChild(card)
+
+    let cover = document.createElement('div')
+    cover.className = "cover"
+    cover.innerHTML = '<img class="img-fluid imgC" src= ' + coverAlbum + '>'
+    card.appendChild(cover)
+
+    let title = document.createElement('h5')
+    title.className = 'title'
+    title.innerText ='Title'
+    card.appendChild(title)
+
+        
+
+    }
+
+    let card = document.createElement('div')
+    card.className = "card"
+    queenSection.appendChild(card)
+
+    let cover = document.createElement('div')
+    cover.className = "cover"
+    card.appendChild(cover)
+
+    let title = document.createElement('h5')
+    title.className = 'title'
+    title.innerText ='Title'
+    card.appendChild(title)
+
+})
+.catch(error => console.log('Something wen wrong = ' + error))
